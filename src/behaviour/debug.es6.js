dd40ebd702efa7df30cbed28daed7a8f1ebef4de
dante <c.dante.federici@gmail.com>
2016-02-14 16:31:47 -0500

    Cleaning up ui/ux of behaviour tool.

--- src/behaviour/debug.es6.js
+++ src/behaviour/debug.es6.js
@@ -8,37 +8,52 @@ var flyd = require('flyd');
 var BehaviourDebugger = function(loadFn, manifest, paused)
 {
 	var render = new dagreD3.render();
+
+	// Build DOM node from template
 	var node = d3.select(document.body)
 		.append('div')
 		.classed('behaviour debug', true);
 
-	// Render structure
 	node.html(ui());
 
-	// Build dag SVG
+	// d3 references to template content
 	var svg = node.select('svg.graph');
 	var svgGroup = svg.append('g');
+	var dotSelect = node.select('.dots');
+
 
 	// Visible toggle
-	var vis = flyd.stream(false);
+	var vis = flyd.stream(true);
 
+
+
+	// Populate select dropdown
+	var dots = ['New Behaviour'].concat(_.keys(manifest));
+	var dotsUp = dotSelect.selectAll('option').data(dots);
+	dotsUp.enter().append('option');
+	dotsUp.text((d) => d);
+
+
+
+
+
+
+
+
+
+
+	// Class node + control game based on visibility
 	flyd.on((isVisible) => {
 		node.classed('hide', !isVisible);
 		paused(isVisible);
 	}, vis);
 
+
+	// UI events
 	node.select('.toggle').on('click', () => {
 		// Toggle vis stream
 		vis(!vis());
 	});
-
-	// @todo: move to menu
-	loadFn('human').then(
-		(res) => {
-			console.log(res);
-			return render(svgGroup, res);
-		}
-	);
 };
 
 module.exports = BehaviourDebugger;
