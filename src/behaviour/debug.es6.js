efc6249518e57f7dd54ff71e03a6b0fad7d98617
dante <c.dante.federici@gmail.com>
2016-02-14 00:40:00 -0500

    Progress to some tool or something...

---
+++ src/behaviour/debug.es6.js
@@ -0,0 +1,12 @@
+'use strict';
+
+var d3 = require('d3');
+var ui = require('./debug.jade');
+
+
+var BehaviourDebugger = function(loader, manifest)
+{
+	console.log(manifest);
+};
+
+module.exports = BehaviourDebugger;
--- src/behaviour/factory.es6.js
+++ src/behaviour/factory.es6.js
@@ -6,21 +6,64 @@ var dot = require('graphlib-dot');
 // @see http://www.graphviz.org/Documentation/dotguide.pdf
 // @see https://github.com/cpettitt/graphlib/wiki/API-Reference#serialization
 // @see https://github.com/cpettitt/graphlib-dot/wiki
+
+
+
+// Manifest of local dot files
+// @todo: generate this based on files
+// @see: https://webpack.github.io/docs/context.html
 var behaviors = {
-	human: require('raw!./human.dot')
+	human: require('raw!./human.dot'),
+	squirrel: require('raw!./squirrel.dot')
 };
+
+
+
+
+
+
 
 var load = (src) => {
 	return new Promise((resolve, reject) => {
 		resolve(dot.read(src));
 	});
-}
+};
 
-load(behaviors.human)
-.then((res) => console.log(res, dot.write(res)))
-.then(
-	undefined,
-	(err) => console.error(err)
-);
 
-module.exports = {};
+
+
+
+
+var _cache = {};
+var loadBehaviour = (name) => {
+	if (!cache[name])
+	{
+		cache[name] = log(behaviors[name]);
+	}
+
+	return cache[name];
+};
+
+
+
+
+
+
+
+// Debug
+var BehaviourDebugger = require('./debug.es6');
+new BehaviourDebugger(loadBehaviour, behaviors);
+
+
+
+
+
+
+
+
+
+module.exports = {
+	behaviors,
+	// Promisified loader
+	load: loadBehaviour
+};
