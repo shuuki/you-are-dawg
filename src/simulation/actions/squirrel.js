111ddf3fe5a7dcba2409921682b48ebd2b4bdaed
dante <c.dante.federici@gmail.com>
2016-02-23 23:30:41 -0500

    Squirrelplosion

--- src/simulation/actions/squirrel.js
+++ src/simulation/actions/squirrel.js
@@ -1,10 +1,15 @@
 'use strict';
+var _ = require('lodash');
+var $ = require('../../core/core.es6');
 
 var Squirrel = {};
 
 // Find the tree, little squirrel
-Squirrel.findTree = (source, land) => {
-	
+Squirrel.findTree = (source, land, delta) => {
+	if (Math.random() > 0.75)
+	{
+		source.pos = $.Vec.sum(source.pos, [_.random(-1, 1), _.random(-1, 1)]);
+	}
 };
 
 module.exports = Squirrel;
